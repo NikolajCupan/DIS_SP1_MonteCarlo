7590407f73f43eee97f435d4a84103e06a2354e4
NikolajCupan <cupannikolaj@gmail.com>
2024-03-08 14:27:59 +0100

    Konstanty ako v dokumentacii

--- src/main/java/org/example/Ostatne/Konstanty.java
+++ src/main/java/org/example/Ostatne/Konstanty.java
@@ -6,8 +6,8 @@ public class Konstanty
     public static final double VYSKA_ISTINY = 100000.0;
     public static final int POCET_ROKOV = 10;
 
-    public static final double PERCENTO_NEZOBRAZENYCH_REPLIKACII = 0.10;
-    public static final double PERCENTO_KROK_AKTUALIZACIA_GRAFU = 0.001;
+    public static final double PERCENTO_NEZOBRAZENYCH_REPLIKACII = 0.20;
+    public static final double PERCENTO_KROK_AKTUALIZACIA_GRAFU = 0.0005;
 
     public static final double ODSADENIE_HRANICNE_HODNOTY = 0.1;
 }
